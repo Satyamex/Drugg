d259989eadd6037caa021118788ea6b951644208
Satyamex <avengersatyam1@gmail.com>
2025-08-17 14:41:05 +0530

    bottle & better piss

--- Assets/Scripts/Credits.cpp
+++ Assets/Scripts/Credits.cpp
@@ -1,3 +1,4 @@
 /* arms: Rigged Fps Arms by Player11132 [CC-BY] via Poly Pizza
- * glock gun: https://mextie.itch.io/low-poly-glock
+ * gun: "Low Poly / Retro / PS1 / PSX USP-S Pistol" (https://skfb.ly/pwTIA) by maxuhhhh is licensed under Creative C * ommons Attribution (http://creativecommons.org/licenses/by/4.0/).
+ * beer bottle: Bottle by Zsky [CC-BY] via Poly Pizza
  * */
